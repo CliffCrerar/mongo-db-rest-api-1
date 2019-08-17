008a5ff762b27e8e501352714c6a8a8b73b05f73
Cliff Crerar <cliff.crerar@gmail.com>
2019-08-17 02:09:54 +0200

    Update

---
+++ api/insertdata/index.js
@@ -0,0 +1,29 @@
+const API_NAME = 'GET DATA';
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
+const Retrieve = require('./_retrieve');
+
+app.use(helmet());
+
+console.log(`|>-> INIT API: ${ API_NAME }`);
+
+app.post('*', (req, res) => {
+    console.log(`|> ${ API_NAME } received request from ${ req.hostname }`);
+    console.log('\n REQUEST HEADERS: ', req.headers, '\n');
+    console.log('\n REQUEST METHOD: ',req.method);
+    Retrieve(req.query,res);
+    //res.status(200).send('yes');
+    //res.send();
+    
+})
+module.exports = app;
+
+const router = express.Router();
+
+router.post('/api/insert',(req,res)=>{
+    console.log('|> REQUEST PASSED TO GET ROUTER!!');
+    try{
+        
+    }
+})
